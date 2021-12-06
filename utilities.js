451ac3e68df900e99aa8dcaf51c73a87e3b88a31
Ben Franco <88692753+BenFranco47@users.noreply.github.com>
2021-12-06 16:19:17 -0500

    Issue #6 part1 Testing

--- utilities.js
+++ utilities.js
@@ -46,3 +46,11 @@ function scored() {
     pawn.y = canvas.height - pawn.height - 40;
 }
 
+//Collision detection between two rectangles
+function collision(first, second){
+    return !(   first.x > second.x + second.width ||
+                first.x + first.width < second.x ||
+                first.y > second.y + second.height ||
+                first.y + first.height < second.y);
+}
+
